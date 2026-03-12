ab9302521155b388560b2e17cb5eef4a242ea011
202230131 <jangyoenun@gmail.com>
2026-03-12 16:30:02 +0900

    week02 v 0.1

--- week02.py
+++ week02.py
@@ -1,3 +1,7 @@
 scores = [100,90,80,70]
-average = sum(scores)/len(scores)
+hap, count = 0,0
+for score in scores:
+    hap = hap + score
+    count = count + 1
+    average = hap / count
 print(average)
