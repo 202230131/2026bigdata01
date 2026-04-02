ef0dc5b9ee5fc68ab43f0af7a5a087221b2ae958
202230131 <jangyoenun@gmail.com>
2026-04-02 16:13:47 +0900

    week05 amusement park app v0.3

--- week05.py
+++ week05.py
@@ -1,15 +1,16 @@
 from asyncio import print_call_graph
 
-#import ticket
+import ticket
 
 humans = int(input("몇 명"))
 ages = list()
 age = 0
 
-for 1 in range(humans);
+for 1 in range(humans):
     age = int(input("나이"))
     ages.append(age)
 
+print(f"총 요금 {ticket.entrance_fee(ages)}원 입니다.")
 print(ages)
 
 
