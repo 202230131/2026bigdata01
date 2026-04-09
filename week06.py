f21d0fbd9b37a17065bb5140f8f2468dc003e101
202230131 <jangyoenun@gmail.com>
2026-04-09 15:56:20 +0900

    week06 v0.1

---
+++ week06.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+array01 = np.random.random((3,3,3))
+print(array01)
+
+print (array01.shape, array01.dtype, array01.ndim, array01.size)
+print (array01.T, array01.dtype, array01.ndim, array01.size)
+
